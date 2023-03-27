10d5bb01a44c4e75c34ce281401e0e0d48341137
rhlz <54817466+rhlz@users.noreply.github.com>
2023-03-27 15:13:14 +0530

    Update hellow.java

--- hellow.java
+++ hellow.java
@@ -5,6 +5,7 @@ public static void mail(String[] args) {
 System.out.println("hellow jenkins");
   System.out.println("hellow jenkins");
   System.out.println("hellow moto");
+  
     System.out.println("hellow moto jay");
 
 
