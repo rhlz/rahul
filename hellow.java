eb8278116394dd32c24b283b0f62a0d67d4d3723
rhlz <54817466+rhlz@users.noreply.github.com>
2023-03-27 13:16:55 +0530

    start
    strat

--- hellow.java
+++ hellow.java
@@ -5,6 +5,9 @@ public static void mail(String[] args) {
 System.out.println("hellow jenkins");
 
 System.out.println("hellow world");
+  System.out.println("hellow start");
+
+System.out.println("hellow id");
 }
 
 }
