c946b331ed1997375aa340c1be8857b8638622f9
rhlz <54817466+rhlz@users.noreply.github.com>
2023-03-27 13:18:49 +0530

    Update hellow.java

--- hellow.java
+++ hellow.java
@@ -4,10 +4,6 @@ public static void mail(String[] args) {
 
 System.out.println("hellow jenkins");
 
-System.out.println("hellow world");
-  System.out.println("hellow start");
-
-System.out.println("hellow id");
 }
 
 }
